a22be4233bf2014502ab7b8b13d6e7aa5a88c1e9
David <davidgonzalezj97@gmail.com>
2024-03-20 14:52:46 +0100

    Exercise 9.1 Implementing console bmi calculator

---
+++ bmiCalculator.ts
@@ -0,0 +1,33 @@
+//BMI uses height in cm and weight in kg
+const calculateBmi = (height: number, weight: number): string => {
+    const heightInMeters = height/100;
+    const bmi = weight/(heightInMeters*heightInMeters);
+
+    switch (true) {
+        case bmi < 16:
+            return "Underweigh (Severe thinness)";
+
+        case bmi >= 16 && bmi < 17:
+            return "Underweight (Moderate thinness)";
+
+        case bmi >= 17 && bmi < 18.5:
+            return "Underweight (Mild thinness)";
+
+        case bmi >= 18.5 && bmi < 25:
+            return "Normal (Healthy weight)";
+
+        case bmi >= 25 && bmi < 30:
+            return "Overweight (Pre-obese)";
+
+        case bmi >= 30 && bmi < 35:
+            return "Obese (Class I)";
+
+        case bmi >= 35:
+            return "Obese (Class II)";
+    
+        default:
+            return "Obese (Class II)";
+    }
+}
+
+console.log(calculateBmi(173, 69))
